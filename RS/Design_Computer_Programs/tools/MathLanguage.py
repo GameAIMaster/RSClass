7829a729499ad3f9fb73ce58d0aefbd3cd44c0f3
fighttogether <1010515634@qq.com>
2020-04-21 00:22:18 +0800

    解析语言联系

--- RS/Design_Computer_Programs/tools/MathLanguage.py
+++ RS/Design_Computer_Programs/tools/MathLanguage.py
@@ -1,4 +1,4 @@
-import string
+import re
 """
 语法：Grammar
 Exp     => Term[+-] Exp | Term
@@ -38,7 +38,46 @@ Exps    => Exp [,] Exps | Exp
 Var     => [a-zA-Z_]\w*
 Num     => [-+]?[0-9]+([.][0-9]*)?""")
 
-print(G)
+
+# >>> parse('Exp', 'a * x', G)
+# (['Exp', ['Term',['Factor', ['Var', 'a']],
+#  '*',
+#  ['Term',['Factor', ['Var', 'x']]]]],'')
+
+
+def parse(start_symbol, text, grammar):
+    """Example call: parse('Exp', '3*x + b, G.
+    Returns a (tree, remainder) pair. if reminder is ''. it parsed the whole
+    string. Failure iff remainder is None.This is a deterministic PEG parser,
+    so rule order (left-to-right) matters. Do 'E => T op E | T', putting the
+    longest parse first(将长的解析放到左侧);don't do 'E=> T | T op E'
+    Also, no left recursion allowed:don't do 'E=> T | T op E'"""
+
+    tokenizer = grammar[' '] + '(%s)'
+    def parse_sequence(sequence, text):
+        result = []
+        for atom in sequence:
+            tree, text = parse_atom(atom, text)
+            if text is None: return Fail
+            result.append(tree)
+        return result, text
+    def parse_atom(atom, text):
+        if atom in grammar: # Non-Terminal: tuple of alternatives
+            for alternative in grammar[atom]:
+                tree, rem = parse_sequence(alternative, text)
+                if rem is not None: return [atom]+tree, rem
+            return Fail
+        else: # Terminal: match characters against start of text 从text中除去带有空格的atom
+            m = re.match(tokenizer % atom, text)
+            return Fail if (not m) else (m.group(1), text[m.end():])
+
+    # Body of parse:
+    return parse_atom(start_symbol, text)
+Fail = (None, None)
+
+print(parse('Exp', 'wq * x + 3', G))
+
+
 
 
 
