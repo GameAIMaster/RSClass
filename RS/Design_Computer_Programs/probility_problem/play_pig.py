eadac3ff25769ea4ee750be85c58e193408694d4
fighttogether <1010515634@qq.com>
2020-05-12 07:56:54 +0800

    寻找游戏胜利的最佳策略，假设对方也会选择最佳

--- RS/Design_Computer_Programs/probility_problem/play_pig.py
+++ RS/Design_Computer_Programs/probility_problem/play_pig.py
@@ -99,7 +99,7 @@ def hold_at(x):
 goal = 50
 
 
-def test():
+def test1():
     assert hold_at(30)((1, 29, 15, 20)) == 'roll'
     assert hold_at(30)((1, 29, 15, 21)) == 'hold'
     assert hold_at(15)((0, 2, 30, 10)) == 'roll'
@@ -107,7 +107,7 @@ def test():
     return 'tests pass'
 
 
-print(test())
+print(test1())
 
 
 def play_pig(A, B):
@@ -138,24 +138,55 @@ def always_hold(state):
     return 'hold'
 
 
-def test1():
+def test2():
     for _ in range(10):
         winner = play_pig(always_hold, always_roll)
         assert winner.__name__ == 'always_roll'
     return 'tests1 pass'
 
 
-print(test1())
+print(test2())
+# -----------------
+# User Instructions
+#
+# Write the max_wins function. You can make your life easier by writing
+# it in terms of one or more of the functions that we've defined! Go
+# to line 88 to enter your code.
+
+from functools import update_wrapper
+
+def decorator(d):
+    "Make function d a decorator: d wraps a function fn."
+    def _d(fn):
+        return update_wrapper(d(fn), fn)
+    update_wrapper(_d, d)
+    return _d
+
+@decorator
+def memo(f):
+    """Decorator that caches the return value for each call to f(args).
+    Then when called again with same args, we can just look it up."""
+    cache = {}
+    def _f(*args):
+        try:
+            return cache[args]
+        except KeyError:
+            cache[args] = result = f(*args)
+            return result
+        except TypeError:
+            # some element of args can't be a dict key
+            return f(args)
+    return _f
 
 # 通过质量函数和效用函数找到最好的执行Action
-
+goal = 40
 def Q_pig(state, action, Pwin):
     "The expect value of choosing action in state"
     if action is "hold":
         return 1 - Pwin(hold(state))
     if action is "roll":
         return (1-Pwin(roll(state,1)) +
-                sum(roll(state, a) for a in (2, 3, 4, 5, 6))) / 6.
+                sum(Pwin(roll(state, a)) for a in (2, 3, 4, 5, 6))) / 6.
     raise ValueError
 
 
@@ -163,3 +194,53 @@ def pig_actions(state):
     "The Legal actions from a state."
     _,_,_,pending = state
     return ["hold","roll"] if pending else ["roll"]
+
+def best_action(state):
+    "Return the optimal action for a state , give U."
+    def EU(action): return Q_pig(state, action, Pwin)
+    return max(pig_actions(state),key=EU)
+
+#包含递归要优化
+@memo
+def Pwin(state):
+    """The utility of a state, here just the probability that an optimal player whose turn it
+     to move can win from the current state"""
+    # Assumes opponent also plays with optimal strategy
+    p, me, you, pending = state
+    if me + pending >= goal:
+        return 1
+    elif you >= goal:
+        return 0
+    else:
+        return max(Q_pig(state, action, Pwin) for action in pig_actions(state))
+
+def max_wins(state):
+    "The optimal pig strategy chooses an action with the highest win probability."
+    return best_action(state)# your code here
+
+
+def test3():
+    assert(max_wins((1, 5, 34, 4)))   == "roll"
+    assert(max_wins((1, 18, 27, 8)))  == "roll"
+    assert(max_wins((0, 23, 8, 8)))   == "roll"
+    assert(max_wins((0, 31, 22, 9)))  == "hold"
+    assert(max_wins((1, 11, 13, 21))) == "roll"
+    assert(max_wins((1, 33, 16, 6)))  == "roll"
+    assert(max_wins((1, 12, 17, 27))) == "roll"
+    assert(max_wins((1, 9, 32, 5)))   == "roll"
+    assert(max_wins((0, 28, 27, 5)))  == "roll"
+    assert(max_wins((1, 7, 26, 34)))  == "hold"
+    assert(max_wins((1, 20, 29, 17))) == "roll"
+    assert(max_wins((0, 34, 23, 7)))  == "hold"
+    assert(max_wins((0, 30, 23, 11))) == "hold"
+    assert(max_wins((0, 22, 36, 6)))  == "roll"
+    assert(max_wins((0, 21, 38, 12))) == "roll"
+    assert(max_wins((0, 1, 13, 21)))  == "roll"
+    assert(max_wins((0, 11, 25, 14))) == "roll"
+    assert(max_wins((0, 22, 4, 7)))   == "roll"
+    assert(max_wins((1, 28, 3, 2)))   == "roll"
+    assert(max_wins((0, 11, 0, 24)))  == "roll"
+    return 'tests3 pass'
+
+print(test3())
+
