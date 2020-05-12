0dc57a763e6ebb1b4a15539c524e7d1ec8c8709e
fighttogether <1010515634@qq.com>
2020-05-13 07:58:01 +0800

    python无法对函数的参数类型进行判断，在使用时应该考虑到边界条件

--- RS/Design_Computer_Programs/probility_problem/play_pig.py
+++ RS/Design_Computer_Programs/probility_problem/play_pig.py
@@ -109,7 +109,7 @@ def test1():
 
 print(test1())
 
-
+#这里用了策略指代不同的玩家
 def play_pig(A, B):
     """Play a game of pig between two players, represented by their strategies.
     Each time through the main loop we ask the current player for one decision,
@@ -124,10 +124,19 @@ def play_pig(A, B):
             return strategies[p]
         elif you >= goal:
             return strategies[other_turn[p]]
-        elif strategies[p](state) == 'hold':
-            state = hold(state)
+        # elif strategies[p](state) == 'hold':
+        #     state = hold(state)
+        # else:
+        #     state = roll(state, random.randint(1,6))
+        # 为了避免无效的策略需要对所有策略判定
         else:
-            state = roll(state, random.randint(1,6))
+            action = strategies[p](state)
+            if action == 'hold':
+                state = hold(state)
+            elif action == 'roll':
+                state = roll(state, random.randint(1,6))
+            else:
+                return strategies[other_turn[p]]
 
 
 def always_roll(state):
@@ -137,12 +146,24 @@ def always_roll(state):
 def always_hold(state):
     return 'hold'
 
+def illegal_strategy(state):
+    return 'I want to win pig please.'
+
+def bad_strategy(state):
+    "A strategy that could never win, unless a player makes an illegal move"
+    return 'hold'
+
+
+
 
 def test2():
     for _ in range(10):
         winner = play_pig(always_hold, always_roll)
         assert winner.__name__ == 'always_roll'
-    return 'tests1 pass'
+
+    winner = play_pig(illegal_strategy, bad_strategy);
+    assert winner.__name__ == 'bad_strategy'
+    return 'tests2 pass'
 
 
 print(test2())
@@ -180,13 +201,13 @@ def memo(f):
 
 # 通过质量函数和效用函数找到最好的执行Action
 goal = 40
-def Q_pig(state, action, Pwin):
+def Q_pig(state, action, U):
     "The expect value of choosing action in state"
     if action is "hold":
-        return 1 - Pwin(hold(state))
+        return 1 - U(hold(state))
     if action is "roll":
-        return (1-Pwin(roll(state,1)) +
-                sum(Pwin(roll(state, a)) for a in (2, 3, 4, 5, 6))) / 6.
+        return (1-U(roll(state,1)) +
+                sum(U(roll(state, a)) for a in (2, 3, 4, 5, 6))) / 6.
     raise ValueError
 
 
@@ -217,6 +238,21 @@ def Pwin(state):
 def max_wins(state):
     "The optimal pig strategy chooses an action with the highest win probability."
     return best_action(state, pig_actions, Q_pig, Pwin)# your code here
+@memo
+def win_diff(state):
+    "The utility of a state: here the winning differential (pos or neg)."
+    (p, me, you, pending) = state
+    if me + pending >= goal or you >= goal:
+        return (me + pending - you)
+    else:
+        return max(Q_pig(state, action, win_diff)
+                   for action in pig_actions(state))
+
+def max_diffs(state):
+    """A strategy that maximizes the expected difference between my final score
+    and my opponent's."""
+    # your code here
+    return best_action(state, pig_actions, Q_pig, win_diff)
 
 
 def test3():
@@ -244,3 +280,24 @@ def test3():
 
 print(test3())
 
+
+def test4():
+    # The first three test cases are examples where max_wins and
+    # max_diffs return the same action.
+    assert(max_diffs((1, 26, 21, 15))) == "hold"
+    assert(max_diffs((1, 23, 36, 7)))  == "roll"
+    assert(max_diffs((0, 29, 4, 3)))   == "roll"
+    # The remaining test cases are examples where max_wins and
+    # max_diffs return different actions.
+    assert(max_diffs((0, 36, 32, 5)))  == "roll"
+    assert(max_diffs((1, 37, 16, 3)))  == "roll"
+    assert(max_diffs((1, 33, 39, 7)))  == "roll"
+    assert(max_diffs((0, 7, 9, 18)))   == "hold"
+    assert(max_diffs((1, 0, 35, 35)))  == "hold"
+    assert(max_diffs((0, 36, 7, 4)))   == "roll"
+    assert(max_diffs((1, 5, 12, 21)))  == "hold"
+    assert(max_diffs((0, 3, 13, 27)))  == "hold"
+    assert(max_diffs((0, 0, 39, 37)))  == "hold"
+    return 'tests4 pass'
+
+print (test4())
